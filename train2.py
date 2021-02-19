dd230ce982566b6b6dd1670fb325989a954bb8d5
chomd90 <chomd90@gmail.com>
2021-02-19 10:06:56 -0500

    fix directory

--- train2.py
+++ train2.py
@@ -19,7 +19,7 @@ from model2 import NetworkCIFAR as Network
 
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='./data', help='location of the data corpus')
 parser.add_argument('--set', type=str, default='cifar10', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
