aa3f1b943dca78ac14c3eca93790737f3beb2402
chomd90 <chomd90@gmail.com>
2021-02-20 12:12:54 -0500

    removed drop_path

--- operations.py
+++ operations.py
@@ -46,14 +46,6 @@ class DilConv(nn.Module):
   def forward(self, x):
     return self.op(x)
 
-def drop_path(x, drop_prob):
-  if drop_prob > 0.:
-    keep_prob = 1.-drop_prob
-    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
-    x.div_(keep_prob)
-    x.mul_(mask)
-  return x
-
 def drop_path_(x, drop_prob, training):
   if training and drop_prob > 0.:
   # if drop_prob > 0.:
