ea2e613a87aa9b0a9f0a83aca762608c0f23b0cc
chomd90 <chomd90@gmail.com>
2021-02-21 20:15:19 -0500

    added the conas subset

--- genotypes.py
+++ genotypes.py
@@ -80,5 +80,8 @@ PC_DARTS_image = Genotype(normal=[('skip_connect', 1), ('sep_conv_3x3', 0), ('se
 
 TEST = Genotype(normal=[('skip_connect', 1), ('dil_conv_3x3', 0), ('avg_pool_3x3', 0), ('dil_conv_3x3', 2), ('sep_conv_3x3', 0), ('skip_connect', 1), ('sep_conv_5x5', 0), ('dil_conv_3x3', 4)], normal_concat=range(2, 6), reduce=[('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 1), ('sep_conv_5x5', 2), ('sep_conv_5x5', 3), ('sep_conv_5x5', 1), ('sep_conv_5x5', 2), ('dil_conv_3x3', 0)], reduce_concat=range(2, 6))
 
-PCDARTS = PC_DARTS_cifar
+SUBSET = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_5x5', 1), ('max_pool_3x3', 0), ('none', 0), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('max_pool_3x3', 1), ('avg_pool_3x3', 2)], normal_concat=range(2, 6), reduce=[('dil_conv_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 1), ('none', 0), ('skip_connect', 2), ('none', 0), ('sep_conv_3x3', 0), ('dil_conv_3x3', 3)], reduce_concat=range(2, 6))
+
+
+PCDARTS = SUBSET
 
