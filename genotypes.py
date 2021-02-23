9b929e2fad573a3ad7ea847f768747d0aa8aa88b
chomd90 <chomd90@gmail.com>
2021-02-23 18:37:57 -0500

    SUBSET2

--- genotypes.py
+++ genotypes.py
@@ -82,6 +82,6 @@ TEST = Genotype(normal=[('skip_connect', 1), ('dil_conv_3x3', 0), ('avg_pool_3x3
 
 # SUBSET = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_5x5', 1), ('max_pool_3x3', 0), ('none', 0), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('max_pool_3x3', 1), ('avg_pool_3x3', 2)], normal_concat=range(2, 6), reduce=[('dil_conv_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 1), ('none', 0), ('skip_connect', 2), ('none', 0), ('sep_conv_3x3', 0), ('dil_conv_3x3', 3)], reduce_concat=range(2, 6))
 SUBSET = Genotype(normal=[('sep_conv_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 1), ('none', 0), ('sep_conv_3x3', 0), ('none', 1), ('sep_conv_3x3', 0), ('avg_pool_3x3', 2)], normal_concat=range(2, 6), reduce=[('max_pool_3x3', 0), ('sep_conv_3x3', 0), ('dil_conv_5x5', 0), ('skip_connect', 1), ('dil_conv_3x3', 0), ('skip_connect', 2), ('skip_connect', 1), ('sep_conv_3x3', 4)], reduce_concat=range(2, 6))
-
-PCDARTS = SUBSET
+SUBSET2 = Genotype(normal=[('max_pool_3x3', 0), ('sep_conv_3x3', 0), ('skip_connect', 1), ('none', 0), ('sep_conv_3x3', 0), ('none', 1), ('sep_conv_3x3', 0), ('avg_pool_3x3', 2)], normal_concat=range(2, 6), reduce=[('sep_conv_3x3', 0), ('dil_conv_3x3', 0), ('dil_conv_5x5', 0), ('skip_connect', 1), ('dil_conv_3x3', 0), ('skip_connect', 2), ('skip_connect', 1), ('sep_conv_3x3', 1)], reduce_concat=range(2, 6))
+PCDARTS = SUBSET2
 
